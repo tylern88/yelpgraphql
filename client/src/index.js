4b24b62849fb27b1a385c6e4d5ab04882dd6475f
tyler <tnegro@gmail.com>
2020-02-16 19:42:22 -0700

    adding initial graphql setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient, { gql, InMemoryCache } from 'apollo-boost'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {YELP_TOKEN} from './env'
+console.log(process.env.YELP_TOKEN)
+
+// when mode is commented out, authorization is displayed in headers, otherwise, it is not
+const cache = new InMemoryCache()
+const client = new ApolloClient({
+  uri: "https://api.yelp.com/v3/graphql",
+  // fetchOptions:{
+  //   mode: "no-cors",
+  // },
+  request: async (operation) => {
+    operation.setContext({
+      mode: "no-cors",
+
+      headers: {
+        // "Authorization": `Bearer ${process.env.YELP_TOKEN}`,
+        "Authorization": `Bearer ${YELP_TOKEN}`,
+        "Content-Type": 'application/graphql',
+        'Accept-Language': 'en-US',
+
+
+      }
+    })
+  },
+  cache: cache
+})
+
+client.query({
+  query: gql`{
+    search(term:"burrito", location:"san francisco") {
+      total
+      business {
+        name
+        reviews {
+          text
+          rating
+          time_created
+          url
+        }
+      }
+    }
+}
+  `
+}
+).then(result => console.log(result))
+.catch(error => console.log(error))
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
