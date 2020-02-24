700fa1176ee7dfdadd1bfa42120ffe773b2fe204
tyler <tnegro@gmail.com>
2020-02-23 21:35:12 -0700

    make fix to server index file

--- server/index.js
+++ server/index.js
@@ -3,15 +3,12 @@ const express = require('express')
 const cors = require("cors")
 const logger = require('morgan')
 import yelpRoutes from './yelp-routes'
-<<<<<<< HEAD
 import { ApolloServer } from 'apollo-server-express'
 import { typeDefs, resolvers } from './graphql'
 
 const server = new ApolloServer({ typeDefs, resolvers })
 
-=======
 import apiRoutes from './routes'
->>>>>>> 429cf7eb042e80e5647deac91a231caaa0152f25
 const app = express()
 server.applyMiddleware({app})
 app.use(
